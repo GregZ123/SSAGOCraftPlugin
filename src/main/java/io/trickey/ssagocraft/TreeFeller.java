638ddc5f50ba581277c960b7d7a188a68cbe7916
GregZ_ <gregz_@gregz.me>
2020-04-03 09:38:41 +0100

    Formatting and conventions
    * Modified the tree feller class to be formatted according to Java standards and to follow oracle Java conventions.

--- src/main/java/io/trickey/ssagocraft/TreeFeller.java
+++ src/main/java/io/trickey/ssagocraft/TreeFeller.java
@@ -12,35 +12,35 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 
 public class TreeFeller implements Listener {
 
-    public static HashSet<Material> logMaterials = new HashSet<>(Arrays.asList(Material.ACACIA_LOG, Material.BIRCH_LOG, Material.DARK_OAK_LOG, Material.BIRCH_LOG, Material.JUNGLE_LOG, Material.OAK_LOG, Material.SPRUCE_LOG));
-    public static HashSet<Material> leafMaterials = new HashSet<>(Arrays.asList(Material.ACACIA_LEAVES, Material.BIRCH_LEAVES, Material.DARK_OAK_LEAVES, Material.BIRCH_LEAVES, Material.JUNGLE_LEAVES, Material.OAK_LEAVES, Material.SPRUCE_LEAVES));
+    private static final HashSet<Material> LOG_MATERIALS = (HashSet<Material>) Collections.unmodifiableSet(new HashSet<>(Arrays.asList(Material.ACACIA_LOG, Material.BIRCH_LOG, Material.DARK_OAK_LOG, Material.BIRCH_LOG, Material.JUNGLE_LOG, Material.OAK_LOG, Material.SPRUCE_LOG)));
+    private static final HashSet<Material> LEAF_MATERIALS = (HashSet<Material>) Collections.unmodifiableSet(new HashSet<>(Arrays.asList(Material.ACACIA_LEAVES, Material.BIRCH_LEAVES, Material.DARK_OAK_LEAVES, Material.BIRCH_LEAVES, Material.JUNGLE_LEAVES, Material.OAK_LEAVES, Material.SPRUCE_LEAVES)));
 
-    protected JavaPlugin plugin;
+    private final JavaPlugin plugin;
 
-    public TreeFeller(JavaPlugin plugin){
+    public TreeFeller(JavaPlugin plugin) {
         this.plugin = plugin;
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onBlockBreak(BlockBreakEvent event){
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public final void onBlockBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
         ItemStack handStack = player.getInventory().getItemInMainHand();
         Material tool = handStack.getType();
-        if(tool == Material.GOLDEN_AXE){
+        if (tool == Material.GOLDEN_AXE) {
             Block block = event.getBlock();
-            if(logMaterials.contains(block.getType())){
+            if (LOG_MATERIALS.contains(block.getType())) {
                 player.sendMessage("*Chop* *Chop* The power of the golden axe fells the tree.");
                 HashSet<Block> logs = new HashSet<>();
                 HashSet<Block> leaves = new HashSet<>();
                 getTree(block, logs, leaves);
                 Material type = logs.iterator().next().getType();
-                for(Block log : logs){
+                for (Block log : logs) {
                     log.setType(Material.AIR);
                 }
                 World world = player.getWorld();
@@ -49,19 +49,18 @@ public class TreeFeller implements Listener {
         }
     }
 
-    protected static void checkDirection(Block anchor, BlockFace direction, HashSet<Block> logs, HashSet<Block> leaves){
+    private void checkDirection(Block anchor, BlockFace direction, HashSet<Block> logs, HashSet<Block> leaves) {
         // North:
         Block nextAnchor = anchor.getRelative(direction);
-        if(logMaterials.contains(nextAnchor.getType()) && !logs.contains(nextAnchor)){
+        if (LOG_MATERIALS.contains(nextAnchor.getType()) && !logs.contains(nextAnchor)) {
             logs.add(nextAnchor);
             getTree(nextAnchor, logs, leaves);
-        }
-        else if(leafMaterials.contains(nextAnchor.getType()) && !leaves.contains(nextAnchor)){
+        } else if (LEAF_MATERIALS.contains(nextAnchor.getType()) && !leaves.contains(nextAnchor)) {
             leaves.add(nextAnchor);
         }
     }
 
-    protected static void checkAllDirections(Block anchor, HashSet<Block> logs, HashSet<Block> leaves){
+    private void checkAllDirections(Block anchor, HashSet<Block> logs, HashSet<Block> leaves) {
         checkDirection(anchor, BlockFace.NORTH, logs, leaves);
         checkDirection(anchor, BlockFace.NORTH_EAST, logs, leaves);
         checkDirection(anchor, BlockFace.EAST, logs, leaves);
@@ -73,9 +72,9 @@ public class TreeFeller implements Listener {
     }
 
 
-    protected static void getTree(Block anchor, HashSet<Block> logs, HashSet<Block> leaves){
+    private void getTree(Block anchor, HashSet<Block> logs, HashSet<Block> leaves) {
         // Limits:
-        if(logs.size() > 150) return;
+        if (logs.size() > 150) return;
 
         checkAllDirections(anchor, logs, leaves);
 
