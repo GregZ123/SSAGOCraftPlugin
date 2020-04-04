6daf968356c7bb2b6c7aea9e14df0acf51e769df
GregZ_ <gregz_@gregz.me>
2020-04-04 09:23:58 +0100

    Main plugin class testing
    * Added dummy values for the TreeFellerListener instantiation to the main class allowing for easy testing.

--- src/main/java/io/trickey/ssagocraft/SSAGOCraftPlugin.java
+++ src/main/java/io/trickey/ssagocraft/SSAGOCraftPlugin.java
@@ -1,11 +1,16 @@
 package io.trickey.ssagocraft;
 
+import io.trickey.ssagocraft.treefeller.TreeFellerListener;
+import org.bukkit.Material;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Collections;
+import java.util.HashSet;
 
 public final class SSAGOCraftPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new TreeFeller(this), this);
+        getServer().getPluginManager().registerEvents(new TreeFellerListener(new HashSet<>(Collections.singletonList(Material.GOLDEN_AXE)), 250, true, 3), this);
     }
 }
